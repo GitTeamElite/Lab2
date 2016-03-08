1218c0dc3adf4bf51c3a4f949b2b30342c6297cc
lundgren84 <lundgren84@hotmail.se>
2016-03-08 10:40:14 +0100

    K

---
+++ GitBlackJack/GitBlackJack/Dealer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBlackJack
+{
+    class Dealer
+    {
+        List<Card> ActiveDeck;
+
+        public int Balance { get; set; }
+
+        Dealer(int balance)
+        {
+            this.Balance = balance;
+            this.ActiveDeck = new List<Card>();
+        }
+        public void GetDeck(List<Card> deck)
+        {
+            foreach (var item in deck)
+            {
+                ActiveDeck.Add(item);
+            }
+        }
+        public void ShuffleActiveDeck()
+        {
+            Random random = new Random();
+            Card cardHolder = new Card();
+
+            foreach (var item in ActiveDeck)
+            {
+                int PointCard = random.Next(ActiveDeck.Count);
+                int pointcard = random.Next(ActiveDeck.Count);
+
+                cardHolder = ActiveDeck[PointCard];
+                ActiveDeck.RemoveAt(PointCard);
+                ActiveDeck.Add(cardHolder);
+            }
+        }
+        public Card GiveCard()
+        {
+            Card card = new Card();
+            card = ActiveDeck[0];
+            ActiveDeck.RemoveAt(0);
+
+            return card;
+
+        } 
+
+    }
+}
