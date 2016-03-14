4cd01d4f7e805b1294d313ea6fce478b41be3781
asvimalo <asvimalo@gmail.com>
2016-03-14 15:57:27 +0100

    a

--- GitChess/GitChess/Player.cs
+++ GitChess/GitChess/Player.cs
@@ -6,20 +6,31 @@ using System.Threading.Tasks;
 
 namespace GitChess
 {
-    class Player :Iplayer
+    class Player 
     {
 
         public List<Piece> IplayPieceList;
-
+        bool _ImBlackPlayer;
 
         public Player()
         {
             //Browse his own all chessboard
             this.IplayPieceList = new List<Piece>();
         }
+
+        //Check if piece choosen is mine + Check legality of the movement ex: A2 --> A4 (Pawn)
         public void MovePiece(int moveX, int moveY, Piece[,]chessBoard )
         {
-            chessBoard[moveX, moveY] = 
+            foreach (var item in chessBoard[Xdärdenstår, Ydärdenstår].AvilibleMoves)//Xdärdenstår,Ydärdenstår introduced by the user in TheGame Class
+            {
+                if ((item.XMove == moveX) && (item.YMove == moveY))                
+                    {
+
+                    }
+
+    
+            }
+            
         }
         public void CheckThretenPieces(Piece[,] chessBoard)
         {
