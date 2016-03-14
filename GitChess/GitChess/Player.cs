607bb2fe11ccf2d1ea46e68338c843fa5aa92b76
lundgren84 <lundgren84@hotmail.se>
2016-03-14 16:07:14 +0100

    x

--- GitChess/GitChess/Player.cs
+++ GitChess/GitChess/Player.cs
@@ -21,6 +21,8 @@ namespace GitChess
         //Check if piece choosen is mine + Check legality of the movement ex: A2 --> A4 (Pawn)
         public void MovePiece(int moveX, int moveY, Piece[,]chessBoard )
         {
+            int Xdärdenstår = 0;
+            int Ydärdenstår = 0;
             foreach (var item in chessBoard[Xdärdenstår, Ydärdenstår].AvilibleMoves)//Xdärdenstår,Ydärdenstår introduced by the user in TheGame Class
             {
                 if ((item.XMove == moveX) && (item.YMove == moveY))                
@@ -32,17 +34,17 @@ namespace GitChess
             }
             
         }
-        public void CheckThretenPieces(Piece[,] chessBoard)
-        {
-            for (int x = 0; x < chessBoard.Length; x++)
-            {
-                for (int y = 0; y < chessBoard.Length; y++)
-                {
-                    if ( )
-                }
-            }
+        //public void CheckThretenPieces(Piece[,] chessBoard)
+        //{
+        //    for (int x = 0; x < chessBoard.Length; x++)
+        //    {
+        //        for (int y = 0; y < chessBoard.Length; y++)
+        //        {
+        //            if ( )
+        //        }
+        //    }
 
-        }
+        //}
         public void CheckAvilibleMoves()
         {
 
